17cbd7fa295b9c33c9459c0bb6421f4e199bc1a6
eric900115 <eric20607@gmail.com>
2023-03-29 13:00:49 +0800

    feat: Google login API

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,16 +1,74 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AfterViewInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const google: any;
 
 @Component({
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent implements OnInit {
-  @ViewChild('name') nameKey!: ElementRef;
-  constructor() {}
+export class WelcomeComponent implements AfterViewInit  {
+  
+  constructor(private router: Router,  private ngZone: NgZone) {}
+ 
+  ngAfterViewInit(): void {
 
-  ngOnInit(): void {}
+    google.accounts.id.initialize({
+      client_id: "752476887018-vvo95p3init8ojkmu7arnkamhmtqrip1.apps.googleusercontent.com",
+      callback: this.startQuiz.bind(this)
+    });
 
-  startQuiz() {
-    localStorage.setItem('name', this.nameKey.nativeElement.value);
+    google.accounts.id.renderButton(
+      document.getElementById("log_in"),
+      {
+        theme: "outline",
+        size: "large",
+        textConfig: {
+          locale: "en"
+        }
+      } // customization attributes
+    );
+
+    google.accounts.id.prompt(); // also display the One Tap dialog
+
+  }
+
+  handleCredentialResponse(response: any) {
+
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    const responsePayload = this.decodeJwtResponse(response.credential);
+
+    let infos = {
+      fullnameL: responsePayload.name,
+      photo_linkL: responsePayload.picture,
+      firstL: responsePayload.given_name,
+      lastL: responsePayload.family_name,
+      mailL: responsePayload.email,
+      id_numL: responsePayload.sub
+    }
+
+    let infosL = JSON.stringify(infos)
+
+    localStorage.setItem("infos", infosL)
+
+    //console.log(infosL);
+  }
+
+  decodeJwtResponse(data: any) {
+    let tokens = data.split(".");
+    return JSON.parse(atob(tokens[1]))
+  }
+
+  navigateToMenu() {
+    this.ngZone.run(() => {
+      this.router.navigateByUrl('/menu');
+    });
+  }
+
+  startQuiz(response: any) {
+    this.handleCredentialResponse(response);
+    this.navigateToMenu();
   }
 }
