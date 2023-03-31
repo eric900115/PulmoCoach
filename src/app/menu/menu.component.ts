8a83bcc46262c6fe9e35a22a867f876d7dd3f24a
ting0602 <ting0602.cs09@ncyu.edu.tw>
2023-03-31 17:36:32 +0800

    fix: web layout

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -13,4 +13,34 @@ export class MenuComponent {
   openPdf(){
     window.open('/assets/Pulmocoach_tutorial.pdf')
   }
+
+  AboutUs = "\n    Medical imaging has been widely used as an \
+examination method in hospitals due to its non-invasive characteristic. \
+Among them, chest radiographs, also known as chest x-rays or CXRs, \
+are the most common. However, there have not been \
+many interactive resources that help medical school \
+students and other healthcare providers learn how \
+to interpret medical images. Therefore, the learning \
+curve for chest radiograph interpretation can be \
+steep and challenging for beginners.\
+            \n\n\
+    Based on this situation, our team, composed of \
+members from departments of computer science, \
+electrical engineering, biomedical engineering, and \
+medicine, decided to build a website that can serve \
+as a tutorial and self-evaluation resource aimed at \
+assisting healthcare providers new to CXR \
+interpretation in better understanding some of the \
+most crucial findings in CXR. \
+                \n\n\
+    Our platform is free and open-source, and our \
+data is also based on open-source atasets. There \
+are plenty of interesting images with accurate \
+lesion localization and annotations made by \
+professional radiologists. So make sure to try \
+it out! \
+\n\n\
+    We are looking forward to incorporating AI \
+diffusion model assistance in the future to make \
+image comparisons and lesion localization easier."
 }
