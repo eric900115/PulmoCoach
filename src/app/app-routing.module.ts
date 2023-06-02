988bcfb9d05f214c07c902108d794517561c02ba
ting0602 <ting0602.cs09@ncyu.edu.tw>
2023-06-02 22:54:26 +0800

    feat: auth init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { MenuComponent } from './menu/menu.component';
 import { QuestionComponent } from './question/question.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HistoryComponent } from './history/history.component';
+import { AuthGuard } from '../app/auth.guard';
 
 const routes: Routes = [
-  {path:"", redirectTo:"welcome", pathMatch:'full'},
-  {path:"welcome", component:WelcomeComponent},
-  {path:"question", component:QuestionComponent},
-  {path:"menu", component:MenuComponent},
-  {path:"history", component:HistoryComponent}
+  {
+    path:"", 
+    redirectTo:"welcome", 
+    pathMatch:'full',
+    canActivate: [AuthGuard],
+    children: [
+      {path:"welcome", component:WelcomeComponent},
+      {path:"question", component:QuestionComponent},
+      {path:"menu", component:MenuComponent},
+      {path:"history", component:HistoryComponent}
+    ]
+  },
 ];
 
 @NgModule({
