e52c0a2c19d8b9fd4e6949b4b9dede08081870db
110062102 <a0909570109@gmail.com>
2023-09-11 02:17:17 +0800

    custom clear function/btn css/input default update

--- src/app/custom-quiz/custom-quiz.component.ts
+++ src/app/custom-quiz/custom-quiz.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-custom-quiz',
   templateUrl: './custom-quiz.component.html',
   styleUrls: ['./custom-quiz.component.scss']
 })
 export class CustomQuizComponent implements OnInit {
-  customAbnormalityRate: number;
-  customQuestionNum: number;
+  customAbnormalityRate: number | undefined;
+  customQuestionNum: number | undefined;
   customGender: string = 'Select';
   @Input() isCustom: boolean = true;
   @Input() isCustomMenuFinished: boolean = true;
@@ -18,9 +18,9 @@ export class CustomQuizComponent implements OnInit {
 
   // selectedSymptom: string = ''; // Initialize selectedSymptom as an empty string
   CQform = new FormGroup({
-    Anomaly_Rate: new FormControl(''),
-    Num_of_Q: new FormControl(''),
-    Gender: new FormControl(''),
+    Anomaly_Rate: new FormControl({}),
+    Num_of_Q: new FormControl({}),
+    Gender: new FormControl({}),
   });
   constructor() { }
 
@@ -36,5 +36,34 @@ export class CustomQuizComponent implements OnInit {
       Symptom: selectedValues
     });
   }
+  customMenuClear() {
+    this.customAbnormalityRate = undefined;
+    this.customQuestionNum = undefined;
+    this.customGender = 'Select';
+    for (const c of this.Symptom) {
+      c.selected = false;
+    }
 
+  }
+
+  // ctrl_input(input_value: number) {
+  //   console.log("car", this.customAbnormalityRate)
+  //   if (input_value > 100) {
+  //     this.customAbnormalityRate = 100;
+  //   }
+  //   else {
+  //     if (Math.round(input_value) < 0) {
+  //       this.customAbnormalityRate = undefined;
+  //     }
+  //     else {
+  //       this.customAbnormalityRate = Math.round(input_value);
+  //     }
+  //   }
+  // }
+
+  disableArrowKeys(event: KeyboardEvent) {
+    // Disable arrow key functionality
+    event.preventDefault();
+  }
 }
+
